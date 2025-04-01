21730c7887b3a09afc7daa9e0696432aed7e7761
Mazen Sheeha <dmazensheha@gmail.com>
2025-04-01 03:59:35 +0200

    Update

--- routes/web.php
+++ routes/web.php
@@ -10,32 +10,41 @@ use App\Http\Controllers\Dashboard\UserController;
 use App\Http\Controllers\Front\FrontController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest:admin')->controller(AuthController::class)->group(function () {
+Route::middleware('redirectIfAuthenticated')->controller(AuthController::class)->group(function () {
     Route::get('/register', 'showRegisterForm')->name('show.register');
     Route::get('/login', 'showLoginForm')->name('show.login');
     Route::post('/login', 'login')->name('login');
     Route::post('/register', 'register')->name('register');
 });
 
-
-Route::prefix('/u')->middleware('auth:web')->controller(FrontController::class)->group(function () {
-    Route::get('/home', 'index')->name('front.index');
-    Route::get('/shop', 'shop')->name('front.shop');
-    Route::get('/contact', 'contact')->name('front.contact');
+Route::middleware('auth:web')->controller(FrontController::class)->group(function () {
+    Route::prefix('/u')->group(function () {
+        Route::get('/home', 'index')->name('front.index');
+        Route::get('/shop', 'shop')->name('front.shop');
+        Route::get('/contact', 'contact')->name('front.contact');
+    });
+    Route::fallback(function () {
+        return redirect('/u/home');
+    });
 });
 
-Route::prefix('/admin')->middleware('auth:admin')->group(function () {
-    Route::get('/', [DasboardController::class, 'dashboard'])->name('dashboard');
-    Route::get("/products/search", [ProductController::class, 'searchByName'])->name("products.searchByName");
-    Route::resource("/products", ProductController::class);
-    Route::get("/products/search/{category}", [ProductController::class, 'searchByCategories'])->name("products.search");
-    Route::get("/categories/{category}/products", [CategoryController::class, 'products'])->name('categories.products');
-    Route::resource("/categories", CategoryController::class);
-    Route::get('admin/orders/info', [OrderController::class, 'ordersInfo'])->name('orders.info');
-    Route::resource("/orders", OrderController::class);
-    Route::get("/users/{user}/orders", [UserController::class, 'orders'])->name('users.orders');
-    Route::resource("/users", UserController::class);
-    Route::resource("/admins", AdminController::class);
+Route::middleware('auth:admin')->group(function () {
+    Route::prefix('/admin')->group(function () {
+        Route::get('/', [DasboardController::class, 'dashboard'])->name('dashboard');
+        Route::get("/products/search", [ProductController::class, 'searchByName'])->name("products.searchByName");
+        Route::resource("/products", ProductController::class);
+        Route::get("/products/search/{category}", [ProductController::class, 'searchByCategories'])->name("products.search");
+        Route::get("/categories/{category}/products", [CategoryController::class, 'products'])->name('categories.products');
+        Route::resource("/categories", CategoryController::class);
+        Route::get('admin/orders/info', [OrderController::class, 'ordersInfo'])->name('orders.info');
+        Route::resource("/orders", OrderController::class);
+        Route::get("/users/{user}/orders", [UserController::class, 'orders'])->name('users.orders');
+        Route::resource("/users", UserController::class);
+        Route::resource("/admins", AdminController::class);
+    });
+    Route::fallback(function () {
+        return redirect('/admin');
+    });
 });
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
