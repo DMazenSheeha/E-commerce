9393584ee6fcb9c77c9da6cfbe2736d4ac6caa85
Mazen Sheeha <dmazensheha@gmail.com>
2025-04-04 02:58:02 +0200

    Update

--- app/Http/Controllers/Dashboard/AdminController.php
+++ app/Http/Controllers/Dashboard/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
